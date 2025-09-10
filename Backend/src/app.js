f85a874cf338597f5e6f806d6d0b997ee4a75e98
Devanshu Tiwaskar <devanshutiwaskar@gmail.com>
2025-09-10 18:00:33 +0530

    fix: Update CORS configuration to allow multiple origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,7 +18,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://ecomm-auth-user-and-seller.vercel.app",
+  origin: [
+  "http://localhost:5173",
+  "https://ecomm-auth-user-and-seller.vercel.app"
+],
   credentials: true
 }));
 
